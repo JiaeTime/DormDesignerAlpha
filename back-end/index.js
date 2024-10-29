d0cbed0187988df7b5ebaf3b84f066ee96d7c9d5
Methasith Reunghirun <96195756+JiaeTime@users.noreply.github.com>
2024-10-29 02:43:10 -0400

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -5,34 +5,33 @@ import { fileURLToPath } from 'url';
 
 const uri = 'mongodb+srv://ananyakoduru22:wJJdnMfiXTaPEyFs@cluster0.pjmeg.mongodb.net/dormDesigner?retryWrites=true&w=majority'; 
 
-async function startServer() {
+async function startServer(req, res) {
   try {
-    await mongoose.connect(uri);
-    console.log("Connected to MongoDB!");
-
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
+    if (!mongoose.connection.readyState) {
+      await mongoose.connect(uri);
+      console.log("Connected to MongoDB!");
+    }
 
     const layoutController = (await import('./controllers/layoutController.js')).default;
 
-    
     const app = express();
-    app.use(express.json()); 
-    const PORT = 5000;
+    app.use(express.json());
 
     app.use('/api', layoutController);
+
+    // Serve static files from React app
     app.use(express.static(path.join(__dirname, '../dorm-designer/build')));
-    
+
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../dorm-designer/build', 'index.html'));
     });
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+    // Handle the request
+    app(req, res);
   } catch (error) {
-    console.error("Error starting server:", error);
+    console.error("Error in serverless function:", error);
+    res.status(500).json({ error: "Server error" });
   }
 }
 
-startServer();
+export default startServer;
